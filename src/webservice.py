d404d4fec211b63ccb1e20d47ee27a7f3d29cf0a
joerg <joerg@yovisto.com>
2021-07-21 15:52:54 +0200

    Update webservice.py

--- src/webservice.py
+++ src/webservice.py
@@ -1,5 +1,4 @@
-import cherrypy, json, sys, cherrypy_cors
-
+import cherrypy, json, sys
 from topic_assistant import TopicAssistant
 
 a = None
@@ -20,7 +19,6 @@ if __name__ == '__main__':
 
    a = TopicAssistant()
 
-   config = {'server.socket_host': '0.0.0.0', 'cors.expose.on': True}
-   cherrypy_cors.install()
+   config = {'server.socket_host': '0.0.0.0'}
    cherrypy.config.update(config)
    cherrypy.quickstart(WebService())	
