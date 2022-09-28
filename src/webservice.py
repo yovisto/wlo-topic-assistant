e453bc21d68b161c7ad7023a36c8f9be282097a3
yovisto <joerg@yovisto.com>
2022-09-28 10:13:39 +0200

    Update webservice.py

--- src/webservice.py
+++ src/webservice.py
@@ -1,7 +1,9 @@
 import cherrypy, json, sys
 from topic_assistant import TopicAssistant
+from topic_assistant2 import TopicAssistant2
 
 a = None
+a2 = None
 
 class WebService(object):
 
@@ -15,9 +17,20 @@ class WebService(object):
       return output
 
 
+@cherrypy.expose
+   @cherrypy.tools.json_out()
+   @cherrypy.tools.json_in()
+   def topics2(self):
+      data = cherrypy.request.json
+      print (data)
+      output = a2.go(data["text"])
+      return output
+
+
 if __name__ == '__main__':
 
    a = TopicAssistant()
+   a2 = TopicAssistant2()
 
    config = {'server.socket_host': '0.0.0.0'}
    cherrypy.config.update(config)
