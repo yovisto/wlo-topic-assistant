b453a1d14865aa7c763f34bc7ded449a74ad67a9
joopitz <jonas.opitz@live.de>
2023-08-03 11:45:00 +0200

    chore: add ping command to web service

--- wlo_topic_assistant/webservice.py
+++ wlo_topic_assistant/webservice.py
@@ -5,37 +5,40 @@ from wlo_topic_assistant.topic_assistant2 import TopicAssistant2
 a = None
 a2 = None
 
+
 class WebService:
+    @cherrypy.expose
+    def _ping(self):
+        pass
 
-   @cherrypy.expose
-   @cherrypy.tools.json_out()
-   @cherrypy.tools.json_in()
-   def topics(self):
-      data = cherrypy.request.json
-      print (data)
-      output = a.go(data["text"])
-      return output
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    def topics(self):
+        data = cherrypy.request.json
+        print(data)
+        output = a.go(data["text"])
+        return output
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    def topics2(self):
+        data = cherrypy.request.json
+        print(data)
+        output = a2.go(data["text"])
+        return output
 
 
-   @cherrypy.expose
-   @cherrypy.tools.json_out()
-   @cherrypy.tools.json_in()
-   def topics2(self):
-      data = cherrypy.request.json
-      print (data)
-      output = a2.go(data["text"])
-      return output
-   
 def main():
-   global a, a2
-   a = TopicAssistant()
-   a2 = TopicAssistant2()
+    global a, a2
+    a = TopicAssistant()
+    a2 = TopicAssistant2()
 
-   # listen to requests from any incoming IP address
-   cherrypy.server.socket_host = "0.0.0.0"
-   cherrypy.quickstart(WebService())
+    # listen to requests from any incoming IP address
+    cherrypy.server.socket_host = "0.0.0.0"
+    cherrypy.quickstart(WebService())
 
 
-if __name__ == '__main__':
-   main()
-
+if __name__ == "__main__":
+    main()
